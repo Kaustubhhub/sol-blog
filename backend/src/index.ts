04c102bcb522aa9f176ce9a540b11587b60147c4
kaustubh <kaustubhkumbhare02@gmail.com>
2025-02-12 07:56:36 +0000

    setup environment variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/",(req,res) => {
-	res.send({message:"The server is healthy"})
+app.get("/", (req, res) => {
+	res.send({ message: "The server is healthy" });
 })
 
-
-app.listen(8000,()=>{
+app.listen(8000, () => {
 	console.log("The server is running on port 8000")
 })
